91fb58f2ea6736798509b7371d2574b24e918d4d
ushkdn <ushkandn@gmail.com>
2023-12-06 15:04:44 +0300

    addded likes in updateDto

--- Dtos/UpdatePostDto.cs
+++ Dtos/UpdatePostDto.cs
@@ -5,6 +5,7 @@
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string[]? Media { get; set; }
+        public int? Likes { get; set; }
         public string[]? Tags { get; set; }
     }
 }   
